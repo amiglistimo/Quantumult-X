16690744468b9fbb775fbf71667b7dacaaeb82f6
52hertz <wenzhao1988@gmail.com>
2022-12-02 13:19:14 +0800

    Update fimo.js

--- Scripts/fimo.js
+++ Scripts/fimo.js
@@ -1,19 +1,12 @@
 /***********************************
 
-> 應用名稱：Fimo
-> 軟件版本：3.0.0
-> 下載地址：https://apps.apple.com/cn/app/id1454219307
-> 腳本作者：Cuttlefish
-> 微信賬號：墨魚手記
-> 更新時間：2022-02-20
-> 通知頻道：https://t.me/ddgksf2021
-> 問題反饋：https://t.me/ddgksf2013_bot
-> 特别說明：本腳本僅供學習交流使用，禁止轉載售賣
+App名称：Fimo
+App版本：v3.0.0
  
 [rewrite_local]
 
-# ～ Fimo解鎖會員權限（2022-02-20）@ddgksf2013
-^https?:\/\/server\.yoyiapp\.com\/fimo-user\/user$ url script-response-body https://ocd0522.tk/ddgksf2013/Cuttlefish/raw/branch/master/Crack/fimo.js
+# ～ Fimo 解锁会员权限
+^https?:\/\/server\.yoyiapp\.com\/fimo-user\/user$ url script-response-body https://raw.githubusercontent.com/amiglistimo/Quantumult-X/main/Scripts/fimo.js
 
 [mitm] 
 
