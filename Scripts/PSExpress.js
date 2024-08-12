9147cf2d90bc12c456198859c288fca0d0d2bc12
52hertz <wenzhao1988@gmail.com>
2024-08-12 14:06:39 +0800

    Update PSExpress.js

--- Scripts/PSExpress.js
+++ Scripts/PSExpress.js
@@ -4,7 +4,7 @@ PSExpress 23.49.1
 2023.12.15
 [rewrite_local]
 
-https://lcs-mobile-cops.adobe.io/mobiles/access_profile/v3 url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/PSExpress.js
+https://lcs-mobile-cops.adobe.io/mobiles/access_profile/v3 url script-response-body https://raw.githubusercontent.com/amiglistimo/Quantumult-X/main/Scripts/PSExpress.js
 
 [mitm] 
 
